4d56573dfba385346a29e4234d9c94c25664abb7
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-02-16 23:33:34 -0800

    generated CRUD paths for categories and subcategories along with the liks for all the info about vinnyslist

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,6 @@
 class CategoriesController < ApplicationController
 
   def index
-    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  
+  resources :categories do
+    resources :subcategories
+  end
+
+  match '/help',    to: 'pages#help',    via: :get
+  match '/scams',   to: 'pages#scams',   via: :get
+  match '/safety',  to: 'pages#safety',  via: :get
+  match '/terms',   to: 'pages#terms',   via: :get
+  match '/privacy', to: 'pages#privacy', via: :get
+  match '/about',   to: 'pages#about',   via: :get
+  match '/contact', to: 'pages#contact', via: :get
+
   root 'categories#index'
+
 end
